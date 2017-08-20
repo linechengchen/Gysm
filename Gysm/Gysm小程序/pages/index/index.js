38675e19544a5e9ea422558af99bae024d2231a0
陈成行 <2480402065@qq.com>
2017-08-20 10:09:51 +0800

    index测试

--- Gysm/Gysm小程序/pages/index/index.js
+++ Gysm/Gysm小程序/pages/index/index.js
@@ -4,7 +4,8 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    banner:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1503183489819&di=0430d7dfedb52affc01bec7bdd112637&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F13%2F41%2F52%2F83n58PICSeV_1024.jpg",
   },
   //事件处理函数
   bindViewTap: function() {
