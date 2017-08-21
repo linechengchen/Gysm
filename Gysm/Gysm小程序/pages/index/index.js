dccf7ba83acb4f6440fcfc265b53399a59541e02
陈成行 <2480402065@qq.com>
2017-08-21 08:49:25 +0800

    2

--- Gysm/Gysm小程序/pages/index/index.js
+++ Gysm/Gysm小程序/pages/index/index.js
@@ -1,27 +1,29 @@
-//index.js
-//获取应用实例
-var app = getApp()
-Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    banner:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1503183489819&di=0430d7dfedb52affc01bec7bdd112637&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F13%2F41%2F52%2F83n58PICSeV_1024.jpg",
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
-})
+var util = require('../../utils/util.js')  
+Page({  
+  data: {  
+    slider: [],  
+    swiperCurrent: 0  
+  },  
+  onLoad: function () {  
+    var that = this;  
+//网络访问，获取轮播图的图片  
+    util.getRecommend(function(data){  
+      that.setData({  
+        slider: data.data.slider  
+      })  
+    });   
+  },  
+  //轮播图的切换事件  
+  swiperChange: function(e){  
+//只要把切换后当前的index传给<swiper>组件的current属性即可  
+    this.setData({  
+      swiperCurrent: e.detail.current  
+    })  
+  },  
+  //点击指示点切换  
+  chuangEvent: function(e){  
+    this.setData({  
+      swiperCurrent: e.currentTarget.id  
+    })  
+  }  
+})  
